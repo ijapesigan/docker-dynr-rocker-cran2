4dcc5fde485da085f79fb01a73a4a8d517f5d879
ijapesigan <ijapesigan@gmail.com>
2025-08-29 23:59:05 +0000

    Automated build.

--- scripts/ubuntu_cran_pkg.sh
+++ scripts/ubuntu_cran_pkg.sh
@@ -6,13 +6,3 @@ install2.r --error --skipinstalled -n -1 \
   devtools        \
   fda             \
   fds
-#  deSolve
-#  fda             \
-#  fds             \
-#  languageserver
-#  latex2exp       \
-#  MASS            \
-#  Matrix
-#  mice            \
-#  numDeriv
-  
